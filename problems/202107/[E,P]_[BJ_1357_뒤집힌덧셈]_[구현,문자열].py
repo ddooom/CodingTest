f262b51f85ebf45e5261c72b1e4c192b61999252
DongWoo <zxcvbnm1997@hanmail.net>
2021-07-18 01:21:40 +0900

    [구현,문자열] BJ_1357_뒤집힌덧셈

--- problems/202107/[E,P]_[BJ_1357_뒤집힌덧셈]_[구현,문자열].py
+++ problems/202107/[E,P]_[BJ_1357_뒤집힌덧셈]_[구현,문자열].py
@@ -1,8 +1,8 @@
 # BJ 1357 뒤집힌 덧셈 <구현, 문자열>
 # https://www.acmicpc.net/problem/1357
-# 시간 : [경과 시간]
-# 문제 리뷰 : [문제 리뷰]
-# 회고 : [회고]
+# 시간 : 5
+# 문제 리뷰 : E, P
+# 회고 : Pythonic하게 구현하기 편리했으며 난이도도 매우 쉬웠다.
 
 a, b = input().split()
-print(str(int(a[::-1])+int(b[::-1]))[::-1])
+print(int(str(int(a[::-1])+int(b[::-1]))[::-1]))
