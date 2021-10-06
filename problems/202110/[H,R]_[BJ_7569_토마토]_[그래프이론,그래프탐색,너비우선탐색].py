b5d6411f9d5e0521dea5656c7e7945bf11bbe83a
DongWoo <zxcvbnm1997@hanmail.net>
2021-10-06 23:10:07 +0900

    [그래프이론,그래프탐색,너비우선탐색] BJ_7569_토마토

--- problems/202110/[H,R]_[BJ_7569_토마토]_[그래프이론,그래프탐색,너비우선탐색].py
+++ problems/202110/[H,R]_[BJ_7569_토마토]_[그래프이론,그래프탐색,너비우선탐색].py
@@ -2,7 +2,7 @@
 # https://www.acmicpc.net/problem/7569
 # 시간 : 60
 # 문제 리뷰 : H,R
-# 회고 : 정답이랑 같은데 나만 틀린다..
+# 회고 : Other Code와 같은 알고리즘인 것 같은데, 나의 코드는 틀리다. 이유를 모르겠다.
 
 
 import sys
@@ -56,3 +56,45 @@ for f in G:
         day = max(day,max(r))
     
 print(day-1)    
+
+
+''' Other Code
+import sys
+from collections import deque
+m,n,h = map(int,input().split()) # mn크기, h상자수
+graph = []
+queue = deque([])
+ 
+for i in range(h):
+    tmp = []
+    for j in range(n):
+        tmp.append(list(map(int,sys.stdin.readline().split())))
+        for k in range(m):
+            if tmp[j][k]==1:
+                queue.append([i,j,k])
+    graph.append(tmp)
+    
+dx = [-1,1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
+while(queue):
+    x,y,z = queue.popleft()
+    
+    for i in range(6):
+        a = x+dx[i]
+        b = y+dy[i]
+        c = z+dz[i]
+        if 0<=a<h and 0<=b<n and 0<=c<m and graph[a][b][c]==0:
+            queue.append([a,b,c])
+            graph[a][b][c] = graph[x][y][z]+1
+            
+day = 0
+for i in graph:
+    for j in i:
+        for k in j:
+            if k==0:
+                print(-1)
+                exit(0)
+        day = max(day,max(j))
+print(day-1)
+'''
