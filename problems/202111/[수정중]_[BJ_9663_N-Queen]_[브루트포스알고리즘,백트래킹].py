927a316e5d3e0ec159b3233c81f49e108bf53463
DongWoo <zxcvbnm1997@hanmail.net>
2021-11-24 23:44:52 +0900

    [브루트포스알고리즘,백트래킹] BJ_9663_N-Queen

--- problems/202111/[수정중]_[BJ_9663_N-Queen]_[브루트포스알고리즘,백트래킹].py
+++ problems/202111/[수정중]_[BJ_9663_N-Queen]_[브루트포스알고리즘,백트래킹].py
@@ -4,50 +4,36 @@
 # 문제 리뷰 : [문제 리뷰]
 # 회고 : [회고]
 
-def marking(row_idx, col_idx, visit):
-    for i in range(N):
-        visit[row_idx][i] = True
-        visit[i][col_idx] = True
-        if 0<=(row_idx + i*1)<N and 0<=(col_idx + i*1)<N:
-            visit[row_idx + i*1][col_idx + i*1] = True
-        if 0<=(row_idx - i*1)<N and 0<=(col_idx + i*1)<N:
-            visit[row_idx - i*1][col_idx + i*1] = True
-        if 0<=(row_idx + i*1)<N and 0<=(col_idx - i*1)<N:
-            visit[row_idx + i*1][col_idx - i*1] = True
-        if 0<=(row_idx - i*1)<N and 0<=(col_idx - i*1)<N:
-            visit[row_idx - i*1][col_idx - i*1] = True
-    return visit
+def possible(row_idx, col_idx, other):
+    if row_idx == other[0]:
+        return False
+    if col_idx == other[1]:
+        return False
+    if abs((row_idx-other[0])/(col_idx-other[1])) == 1:
+        return False
+    return True
 
 
 def search(row_idx, col_idx, visit):
     global answer
-#     for i in range(N):
-#         print(visit[i])
-#     print()
     if row_idx == (N-1):
         answer += 1
         return 0
     
-    if col_idx-2 >= 0:
-        for i in range(col_idx-2, -1, -1):
-            if not visit[row_idx+1][i]: 
-                tmp_visit = visit.copy()
-                tmp_visit = marking(row_idx+1, i, tmp_visit)
-                search(row_idx+1, i, tmp_visit)
-    
-    if col_idx+2 < N:
-        for i in range(col_idx+2, N):
-            if not visit[row_idx+1][i]:
-                tmp_visit = visit.copy()
-                tmp_visit = marking(row_idx+1, i, tmp_visit)
-                search(row_idx+1, i, tmp_visit)
-    
+    for i in list(range(0, col_idx-1)) + list(range(col_idx+2, N)):
+        recur = True
+        for j in range(0, len(visit)-1):
+            if not possible(row_idx+1, i, visit[j]):
+                recur = False
+                break
+        if recur:
+            search(row_idx+1, i, visit + [[row_idx+1, i]])
+
 
 N = int(input())
 answer = 0
 for col_idx in range(N):
-    visit = [[False for _ in range(N)] for _ in range(N)]
-    visit = marking(0, col_idx, visit)
-    search(0, col_idx, visit)
-
+    visit = []
+    search(0, col_idx, [[0, col_idx]])
+    
 print(answer)
