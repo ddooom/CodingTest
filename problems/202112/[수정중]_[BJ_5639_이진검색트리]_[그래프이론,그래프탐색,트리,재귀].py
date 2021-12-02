5451af0db73ec8287cbc650892dc81b1db812ed3
DongWoo <zxcvbnm1997@hanmail.net>
2021-12-02 23:57:44 +0900

    [그래프이론,그래프탐색,트리,재귀] BJ_5639_이진검색트리

---
+++ problems/202112/[수정중]_[BJ_5639_이진검색트리]_[그래프이론,그래프탐색,트리,재귀].py
@@ -0,0 +1,9 @@
+# BJ 5639 이진 검색 트리 <그래프 이론, 그래프 탐색, 트리, 재귀>
+# https://www.acmicpc.net/problem/5639
+# 시간 : [경과 시간]
+# 문제 리뷰 : [문제 리뷰]
+# 회고 : [회고]
+
+import sys
+a = [*sys.stdin.readlines().strip()
+print(a)
